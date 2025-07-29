c14094e16ed90f4c8cf5f5c0e414e1d5c180252f
NgawangNorbu123 <ngawangznorvu@gamil.com>
2025-07-29 14:41:30 +0600

    new program

--- First program.py
+++ First program.py
@@ -1,2 +1,6 @@
-print("hello SWE")
-
+'''A=11
+B=12
+print("the sum of A and b is :",A+B)'''
+name=input("ENter your name:")
+age=input("Enter your age :")
+print("you are" +name+ "and you are" +age+ "years old")
