adbd1644a8b394f0767d4d3a6be89d46fc54c815
NgawangNorbu123 <ngawangznorvu@gamil.com>
2025-08-19 11:45:49 +0600

    scope.py

---
+++ CSF101-python_EXERCISE/functions_and _scope/scope.py
@@ -0,0 +1,30 @@
+x = 10 # Global variable
+def print_x():
+    x =20 
+    print(f"Local x: {x}")
+
+print_x()
+print(f"Global x: {x}")
+
+count = 0 
+def increment():
+    global count
+    count += 1
+    print(f"Count: {count}")
+
+increment()
+increment()
+increment()
+print(f"Final count: {count}")
+
+def outer():
+    x = "outer"
+
+    def inner():
+        nonlocal x
+        x = "inner"
+        print(f"Inner x: {x}")
+
+    inner()
+    print(f"outer x: {x}")
+outer()
